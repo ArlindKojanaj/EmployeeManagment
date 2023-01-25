619d64b3ea65632b14f505fd171d91ffe782ce7b
Mario Perna <mario.perna@ict-group.it>
2023-01-25 18:52:13 +0100

    WIP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,10 @@ import Config from '../config.json';
 export const keycloakConfig: KeycloakConfig = {
   url: RESOLVE_ENV(Config.SSO_URL),
   realm: RESOLVE_ENV(Config.SSO_REALM),
-  clientId: RESOLVE_ENV(Config.SSO_CLIENTID),
+  clientId: RESOLVE_ENV(Config.SSO_CLIENTID)
 };
+
+export const api = RESOLVE_ENV(Config.API_URL);
 
 export const environment = {
   production: false,
