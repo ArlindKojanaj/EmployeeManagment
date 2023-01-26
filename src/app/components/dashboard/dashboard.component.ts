3c7691adc6dc3991de5d10f3beb5703f9898c3cf
Mario Perna <mario.perna@ict-group.it>
2023-01-26 10:22:12 +0100

    POC endpoint

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from 'src/app/core/services/employee.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployeeCount();
+  }
+
+  getEmployeeCount() {
+    this.employeeService.count().subscribe(
+      c => {
+        console.log('count', c);
+      }
+    );
+
+  }
 }
---
+++ src/app/core/services/employee.service.ts
@@ -0,0 +1,61 @@
+import {Injectable} from '@angular/core';
+import {HttpBackend, HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+// import {Commessa} from '../model/commessa';
+import {map} from 'rxjs/operators';
+import { api } from 'src/environments/environment';
+
+export const COMMESSE = 'https://panta.coopservice.it/asset-rs/assets/commesse';
+export const EMPLOYEE = `${api}/employee`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+
+  constructor(protected http: HttpClient, private httpBackend: HttpBackend) {
+  }
+
+
+//   public commesseNoIntercpetor(): Observable<Commessa []> {
+//     const newHttpClient = new HttpClient(this.httpBackend);
+//     return newHttpClient.get<HttpResponse<Commessa []>>(COMMESSE, {
+//       observe: 'response',
+//     }).pipe(
+//       map(res => {
+//           const ts: any = res.body;
+//           return ts;
+//         }
+//       )
+//     );
+//   }
+
+  public count(): Observable<Number> {
+    return this.http.get<HttpResponse<Number>>(`${EMPLOYEE}/count`, {
+      observe: 'response',
+    }).pipe(
+      map(res => {
+          const ts: any = res.body;
+          return ts;
+        }
+      )
+    );
+  }
+
+//   public commesseSize(): Observable<number> {
+//     let params = new HttpParams();
+//     params = params.set('pageSize', '1');
+//     return this.http.get<HttpResponse<number>>(COMMESSE, {
+//       observe: 'response',
+//       params
+//     }).pipe(
+//       map(res => {
+//           console.log('headers', res.headers.keys());
+//           return +res.headers.get('listsize');
+//         }
+//       )
+//     );
+//   }
+
+}
