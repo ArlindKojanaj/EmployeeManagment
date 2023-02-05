268c6a5b02f74658e777be9654f4f53c1dbd5949
Arlind Kojanaj <kojaniarlind1@gmail.com>
2023-02-05 13:19:12 +0100

    setting up new epm form

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss']
 })
+
 export class AddComponent {
+
+ constructor(private ls:ListComponent){
+  
+ }
+onclose(){
+  this.ls.addmode=false
+
+}
 
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -3,16 +3,19 @@ import { AuthService } from 'src/app/core/services/auth.service';
 import { EmployeeService } from 'src/app/core/services/employee.service';
 import { MenuItem } from 'primeng/api';
 import { Employe } from 'src/app/core/services/employe';
+import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
+@Injectable()
 export class ListComponent {
   employe:Employe[];
-items: MenuItem[];
+  items: MenuItem[];
   itemsL: MenuItem[];
   activeItem: MenuItem;
+  addmode:boolean=false
 
   constructor(private employeeService: EmployeeService, private authService: AuthService) { 
     this.items = []; 
@@ -47,6 +50,10 @@ items: MenuItem[];
 
   logout() {
     this.authService.logout();
+  }
+
+  showform() {
+    this.addmode=true
   }
 
 
