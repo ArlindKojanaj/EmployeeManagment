9d1f8819ea55a7ecb19eea794dd8c25c598dcec5
Arlind Kojanaj <kojaniarlind1@gmail.com>
2023-02-06 17:41:45 +0100

    add employe fix

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ListComponent } from '../list/list.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from 'src/app/core/services/employee.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -7,14 +10,41 @@ import { ListComponent } from '../list/list.component';
   styleUrls: ['./add.component.scss']
 })
 
-export class AddComponent {
+export class AddComponent implements OnInit {
 
- constructor(private ls:ListComponent){
-  
+ employeeForm!:FormGroup;
+
+ constructor(private ls:ListComponent,private empService:EmployeeService,private router:Router){}
+
+ ngOnInit() {
+   this.initForm()
  }
+
+
 onclose(){
   this.ls.addmode=false
-
+  
 }
 
+onSubmit(){
+  this.empService.addEmployee(this.employeeForm.value)
+  // this.router.navigate(['/home']);
+  // this.ls.addmode=false
+}
+
+
+private initForm() {
+  let firstName = '';
+  let lastName = '';
+  let dob = '';
+  let email=''
+
+  this.employeeForm = new FormGroup({
+    firstName: new FormControl(firstName, Validators.required),
+    lastName: new FormControl(lastName, Validators.required),
+    dob: new FormControl(dob, Validators.required),
+    email: new FormControl(email, Validators.required),
+    
+  });
+}
 }
