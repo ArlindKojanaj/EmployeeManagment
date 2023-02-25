4241669239a2723cae719e911ff7403e5e7df7a3
Arlind Kojanaj <kojaniarlind1@gmail.com>
2023-02-25 18:03:58 +0100

    update app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AppAuthGuard } from './init/auth.authguard';
 import { ListComponent } from './components/list/list.component';
 import { AddComponent } from './components/add/add.component';
 import { ListEmployeeDetailComponent } from './components/list/list-employee-detail/list-employee-detail.component';
+import { DeleteEmployeeModalComponent } from './components/list/delete-employee-modal/delete-employee-modal.component';
 
 const routes: Routes = [
   {
@@ -30,7 +31,10 @@ const routes: Routes = [
       path:':id',component:ListEmployeeDetailComponent,canActivate:[AppAuthGuard]
     },{
       path:':id/edit',component:AddComponent,canActivate:[AppAuthGuard]
-    }]
+    },{
+      path:':id/delete',component:DeleteEmployeeModalComponent,canActivate:[AppAuthGuard]
+    }
+  ]
   }
 ];
 
