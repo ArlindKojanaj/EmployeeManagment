acf1cc2a5a1dfedab7687410d9a5b4750e234174
Arlind Kojanaj <kojaniarlind1@gmail.com>
2023-02-28 11:42:28 +0100

    working on delete form regdate and regperiod

--- src/app/components/list/delete-employee-modal/delete-employee-modal.component.ts
+++ src/app/components/list/delete-employee-modal/delete-employee-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Employe } from 'src/app/core/services/employe';
 import { EmployeeService } from 'src/app/core/services/employee.service';
@@ -9,11 +10,13 @@ import { EmployeeService } from 'src/app/core/services/employee.service';
   styleUrls: ['./delete-employee-modal.component.scss']
 })
 export class DeleteEmployeeModalComponent {
+
+deleteForm!:FormGroup;
 employe: Employe;
 id:number;
    
   constructor(private route:ActivatedRoute,
-              private emplyeeService:EmployeeService,
+    private emplyeeService:EmployeeService,
             private router:Router ){}
 
   ngOnInit(): void {
@@ -21,17 +24,42 @@ id:number;
     this.id=+params['id']
     this.employe=this.emplyeeService.getEmploye(this.id)
     
+    this.initForm()
     })
+  }
+
+  onSubmit(){
+    this.emplyeeService.deleteEmployee(this.id);
+    this.router.navigate(['/list']);
+    console.log(this.deleteForm.value)
+
   }
 
   onNavigate(){
     this.router.navigate(['/list'])
 }
 
-onDeleteRecipe() {
-  this.emplyeeService.deleteEmployee(this.id);
-  this.router.navigate(['/list']);
+// onDeleteRecipe() {
+//   this.emplyeeService.deleteEmployee(this.id);
+//   this.router.navigate(['/list']);
 
+// }
+
+private initForm() {
+  let id = 0;
+  let regdate = '';
+  let period=0;
+
+
+  this.deleteForm = new FormGroup({
+   
+    id: new FormControl(this.id, Validators.required),
+    regdate: new FormControl(regdate, Validators.required),
+    period: new FormControl(period, Validators.required),
+    
+    
+  });
+  
 }
 
 }
