6fe9da991e6c8315dac8dca4319e1efe156cbd71
Arlind Kojanaj <kojaniarlind1@gmail.com>
2023-03-01 14:14:08 +0100

    fix on history rows

--- src/app/core/services/employe.ts
+++ src/app/core/services/employe.ts
@@ -7,13 +7,29 @@ export class Employe {
     public  dob: number
     public email: string
     public phone:number
+    public  note: string
+    public startDate: number
+    public  endDate: number
+    public repeatEvery: number
 
-    constructor(firstName: string, laName: string, dbirth: number, emaiil:string,phonee:number) {
+    constructor(firstName: string, 
+                laName: string,
+                dbirth: number, 
+                emaiil:string,
+                phonee:number,
+                notee:string,
+                startDate:number,
+                endDate:number,
+                repeatEvery:number) {
       this.firstName =firstName ;
       this.lastName = laName;
       this.dob = dbirth;
       this.email = emaiil;
-      this.phone=phonee
+      this.phone=phonee;
+      this.note=notee;
+      this.startDate=startDate;
+      this.endDate=endDate;
+      this.repeatEvery=repeatEvery
     }
     // "address": string,
     // "city": string,
